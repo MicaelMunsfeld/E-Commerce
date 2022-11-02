78fde171c7a327012c0ae449380f1c8c8ed028d5
MicaelMunsfeld <106362082+MicaelMunsfeld@users.noreply.github.com>
2022-11-02 20:56:15 -0300

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ require 'conection.php';
 
 	<!-- CSS Aqui -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="style.css?v=1.1">
+	<link rel="stylesheet" type="text/css" href="style.css?v=1.2">
 
 </head>
 <body class="bg-gray">
@@ -23,52 +23,52 @@ require 'conection.php';
 		<div class="col-md-12">
 			<div class="card">
 				<div class="card-header p-5 bg-red">
-					<h4 class="text-center text-white">E-COMMERCE - SISTEMAS DE INFORMA«√O - FASE 4</h4>
+					<h4 class="text-center text-white">E-COMMERCE - SISTEMAS DE INFORMA√á√ÉO - FASE 4</h4>
 				</div>
 			</div>
 		</div>
 	</div>
 	<section class="container mt-5 mb-5">
 		<div class="row">
-			<div class="col-3 bg-red p-5">
+			<div class="col-3 bg-red p-5 br-10">
 				<div>
-                                    <?php 
-                                        $oQuery     = "SELECT * FROM categorias";
-                                        $oQuery_run = mysqli_query($oConexao, $oQuery);
+					<?php 
+					$oQuery     = "SELECT * FROM categorias";
+					$oQuery_run = mysqli_query($oConexao, $oQuery);
 
-                                        foreach($oQuery_run as $oCategoria) {
-                                            ?>
-                                                <ul>
-                                                    <a class="no-decoration" href="#">
-                                                        <li class="text-white"><?= $oCategoria['descricao']; ?></li>
-                                                    </a>
-                                                </ul>
-                                            <?php
-                                        }
-                                    ?>
+					foreach($oQuery_run as $oCategoria) {
+						?>
+						<ul>
+							<a class="no-decoration" href="view_categoria.php?id=<?= $oCategoria['id']; ?>">
+								<li class="text-white"><?= $oCategoria['descricao']; ?></li>
+							</a>
+						</ul>
+						<?php
+					}
+					?>
 				</div>
 			</div>
-			<div class="col-9 p-5">
-                            <div class="row">
-                                    <?php 
-                                        $oQuery     = "SELECT * FROM produtos";
-                                        $oQuery_run = mysqli_query($oConexao, $oQuery);
+			<div class="col-9 p-3">
+				<div class="row">
+					<?php 
+					$oQuery     = "SELECT * FROM produtos";
+					$oQuery_run = mysqli_query($oConexao, $oQuery);
 
-                                        foreach($oQuery_run as $oProduto) {
-                                            ?>
-                                            <div class="card col-4">
-                                                <img class="img-height-produto" src="<?= $oProduto['imagem']; ?>" alt="<?= $oProduto['descricao']; ?>">
-                                                <div class="card-body">
-                                                        <h5 class="card-title"><?= $oProduto['descricao']; ?></h5>
-                                                        <p class="card-text text-height-produto"><?= substr($oProduto['caracteristicas'], null, 160) . '...'; ?></p>
-                                                        <a href="#" class="btn btn-primary w-100">Ver mais</a>
-                                                </div>
-                                            </div>
-                                            <?php
-                                        }
-                                    ?>
-                            </div>
-                        </div>
+					foreach($oQuery_run as $oProduto) {
+						?>
+						<div class="card col-4 br-10">
+							<img class="img-height-produto br-10" src="<?= $oProduto['imagem']; ?>" alt="<?= $oProduto['descricao']; ?>">
+							<div class="card-body">
+								<h5 class="card-title"><?= $oProduto['descricao']; ?></h5>
+								<p class="card-text text-height-produto"><?= substr($oProduto['caracteristicas'], null, 100) . '...'; ?></p>
+								<a href="view_produto.php?id=<?= $oProduto['id']; ?>" class="btn btn-primary w-100">Ver mais</a>
+							</div>
+						</div>
+						<?php
+					}
+					?>
+				</div>
+			</div>
 		</div>
 	</section>
 
